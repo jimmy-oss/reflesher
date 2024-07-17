42261eb18c51eccae6dd225a368e1a0b71016885
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-17 14:08:52 +0300

    operators

---
+++ operators/script.js
@@ -0,0 +1,17 @@
+//!  Arithmetic Operators
+// Addition operator (+) adds two numbers together
+const applesOnHand = 5;
+const applesReceived = 10;
+const totalApples = applesOnHand + applesReceived;
+ 
+// Subtraction operator (-) subtracts two numbers together
+const totalTickets = 100;
+const ticketSold = 45;
+const ticketLeft = totalTickets - ticketSold;
+
+// multiplication operator (*) multiplies two numbers together
+const bookCosts = 20;
+const bookBought = 15;
+const totalBookPrice = bookCosts * bookBought;
+
+// Division operator (/) divides two numbers together
