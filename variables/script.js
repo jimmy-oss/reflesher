0147f09d8f0efd25376c24000cca383b9355337c
jimmy-oss <jimmynjonge51@gmail.com>
2024-07-17 12:12:05 +0300

    learnt datatypes

--- variables/script.js
+++ variables/script.js
@@ -2,4 +2,30 @@
 var myAge = 20;
 let myMoney = 100;
 const myCity = 'Nairobi';
-console.log(myCity)
+ 
+//! Primitive Data Types
+// String
+let fullName = "John Doe";
+ 
+// Number
+let accountBalance = 200;
+ 
+// Boolean
+let isMarried = true;
+
+// Null
+let pendingTransaction = null;
+
+// Undefined
+let country = undefined;
+
+// What does null and undefined means in javascript 
+// Both null and undefined represents absence of values
+// But they are used in different circumstances
+// For example undefined is value that has been declared
+// that has no assigned value and by default it will give
+// you undefined
+// null is an assignment value that has been assigned to a variable
+// that represents no value or no object and it implies an empty
+// or a non-existing value so in short they are both empty when a variable
+// is declared but they have some differences is some situations.
