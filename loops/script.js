08d82dbfe200f9597401bc7f67fdc126061942ee
jimmy-oss <jimmynjonge51@gmail.com>
2024-08-08 19:14:48 +0300

    for loops

---
+++ loops/script.js
@@ -0,0 +1,12 @@
+// for loop
+// display 0-100 to the console
+
+// for ('Initialisation'; 'condition'; 'increment'){
+  
+//}
+// steps
+// initialize
+let i;
+for (i=0; i<=5; ++i){
+  console.log(i);
+}
