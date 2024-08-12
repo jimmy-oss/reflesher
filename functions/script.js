6d658e5f85abd27c0cd5036f5067393fe2162de2
jimmy-oss <jimmynjonge51@gmail.com>
2024-08-12 16:49:22 +0300

    functions

---
+++ functions/script.js
@@ -0,0 +1,13 @@
+// Ways of creating a function
+//1. function declaration
+// 2. function expression
+
+// Create a function to say hello
+// Function Declaration
+// Create a Function
+function sayHello() {
+  console.log('Hello Welcome To Function');
+  
+}
+// Calling a Function || Invocation
+sayHello()
