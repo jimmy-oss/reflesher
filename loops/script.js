351e3213c8a91a2551e2c98f6037bf36fc13a147
jimmy-oss <jimmynjonge51@gmail.com>
2024-08-12 15:56:45 +0300

    loops

--- loops/script.js
+++ loops/script.js
@@ -6,6 +6,10 @@
 //}
 // steps
 // initialize
+// The **initialization** expression initializes the loop; 
+// it's executed once, as the loop begins.;
+// When the **condition** returns true, the loop executes the statement.
+// The **final-expression** is executed every time the statement (s) runs.
 let i;
 for (i=0; i<=5; ++i){
   console.log(i);
